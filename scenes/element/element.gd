28c55f5f9854cdc377ab037d5bb0bfff091fee83
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 17:25:47 +0100

    Add Element class to represent items in cells

---
+++ scenes/element/element.gd
@@ -0,0 +1,13 @@
+class_name Element
+extends Node2D
+
+var category: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
