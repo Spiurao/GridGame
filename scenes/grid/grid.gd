02946dd82b94bbd51aaff3dd899035a49c6721c2
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 18:28:30 +0100

    Implement element dropping

--- scenes/grid/grid.gd
+++ scenes/grid/grid.gd
@@ -52,9 +52,52 @@ func delete_same_type_adjacent_elements(coordinates: Vector2i, element_type: Str
 		delete_same_type_adjacent_elements(coordinates + Vector2i(0,1), element_type, visited)
 		delete_same_type_adjacent_elements(coordinates + Vector2i(0,-1), element_type, visited)
 
+func drop_elements() -> void:
+	for i in range(width-1, -1, -1):
+		for j in range(height-1, -1, -1):
+			if get_cell(Vector2i(i,j)).is_empty:
+				# Making all elements above fall down
+				var k := j
+				var empty_column_size := 0
+				var found_element := false
+				var found_next_empty_space := false
+
+				var hanging_cell: Cell = null
+				while k > 0 and not found_element: # checking if something needs to fall down
+					k -= 1
+					empty_column_size += 1
+					hanging_cell = get_cell(Vector2i(i,k))
+					found_element = not hanging_cell.is_empty
+
+					if found_element: # find the end of the block of elements that will fall
+						var elements_block_size := 0
+						var l: int
+
+						# edge case where we check the element on top
+						if k == 0:
+							found_next_empty_space = true
+							l = -1
+						else:
+							l = k
+
+						# this logic is eventually repeated for
+						# each block of elements separated
+						# by spaces in the current column
+						while l > 0 and not found_next_empty_space:
+							l -= 1 #-1 so k-l = 1 meaning we have only the top element to move
+							found_next_empty_space = get_cell(Vector2i(i,l)).is_empty
+
+						if found_next_empty_space or l == 0: # l=0 means we've reached the top
+							elements_block_size = k-l
+							for m in range(elements_block_size):
+								get_cell(Vector2i(i,j-m))\
+								.swap_elements(get_cell(Vector2i(i,j-empty_column_size-m)))
+
 func get_cell(coordinates: Vector2i) -> Cell:
 	return get_node_or_null("%s" % coordinates)
 
 #TODO implement behavior when a cell is clicked
 func _cell_clicked(cell: Cell) -> void:
 	delete_same_type_adjacent_elements(cell.coordinates, cell.get_element().category, {})
+	await get_tree().process_frame #TODO delete (is for debugging purposes)
+	drop_elements()
