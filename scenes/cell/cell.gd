6e53aef1140e4a60ca4ca2763388aabed314a11f
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 18:27:34 +0100

    Add element swapping method in Cell

--- scenes/cell/cell.gd
+++ scenes/cell/cell.gd
@@ -34,6 +34,25 @@ func delete_element() -> void:
 	$Element.queue_free()
 	is_empty = true
 
+## Swaps two elements and triggers the movement animation
+func swap_elements(cell: Cell) -> void:
+	var old_element := get_element()
+	var new_element := cell.get_element()
+
+	if new_element:
+		cell.remove_child(new_element)
+		add_child(new_element)
+		# moving it back to the old position so it doesn't teleport
+		new_element.global_position = cell.global_position
+		new_element.move_to(global_position)
+	if old_element:
+		remove_child(old_element)
+		cell.add_child(old_element)
+		old_element.global_position = global_position
+		old_element.move_to(cell.global_position)
+
+	is_empty = get_element() == null
+	cell.is_empty = cell.get_element() == null
 
 func _on_area_2d_input_event(_viewport, event, _shape_idx) -> void:
 	if event.is_action_pressed("click") and get_element():
