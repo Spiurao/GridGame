0700ea2b73f4f04b41cb5ad7f461593d5b54fdfd
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 17:53:28 +0100

    Implement random grid generation

--- scenes/grid/grid.gd
+++ scenes/grid/grid.gd
@@ -1,14 +1,27 @@
 class_name Grid
 extends Node2D
 
+const SEPARATOR_WIDTH = 4
+
 @export var height := 10
 @export var width := 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	generate_random_grid() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
+
+func generate_random_grid() -> void:
+	for i in range(width):
+		for j in range(height):
+			var cell := SceneFactory.create_cell(Vector2i(i,j), SEPARATOR_WIDTH, _cell_clicked)
+
+			add_child(cell)
+
+#TODO implement behavior when a cell is clicked
+func _cell_clicked(cell: Cell) -> void:
 	pass
