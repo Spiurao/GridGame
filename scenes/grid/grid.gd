274cef1fbfb13a7c717b2f6c0589de6cbca7a201
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 17:19:14 +0100

    Add basic grid structure

---
+++ scenes/grid/grid.gd
@@ -0,0 +1,14 @@
+class_name Grid
+extends Node2D
+
+@export var height := 10
+@export var width := 8
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
