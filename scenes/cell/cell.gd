68c52ef49b522b91f52319c54238d0f0b2872f6a
Antoine MAGNIN <a.magnin44@gmail.com>
2026-01-16 17:42:22 +0100

    Add initialization methods to Cell

--- scenes/cell/cell.gd
+++ scenes/cell/cell.gd
@@ -8,6 +8,22 @@ var is_empty := true
 func _ready():
 	pass # Replace with function body.
 
+func initialize(coord: Vector2i, separation_offset: int, _on_clicked_callback: Callable) -> void:
+	var element := SceneFactory.generate_random_element()
+	initialize_with_element(coord, element, separation_offset, _on_clicked_callback)
+
+func initialize_with_element(coord: Vector2i, element: Element, separation_offset: int,\
+			_on_clicked_callback: Callable) -> void:
+	connect("cell_clicked", _on_clicked_callback)
+
+	# Offset position within grid
+	position = coord * (SceneFactory.SPRITE_SIZE + separation_offset)
+
+	coordinates = coord
+	element.name = "Element"
+
+	is_empty = false
+	add_child(element)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
